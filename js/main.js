a8518b99013cd5b21c72cbe5eb2623d7dc0bf7e5
Clara Pasteau <clarapasteau@gmail.com>
2020-09-08 17:59:35 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+	// code that was prewritten probably for an arrow
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1){  
+			$('.page-title').addClass("sticky");
+		}
+		else{
+			$('.page-title').removeClass("sticky");
+		}
+	});
+
+
+    $('.minus').hide();
+    $('.captions').hide();
+    $(".plus").click(function(){
+        $(this).parents().next('.captions ').slideToggle();
+        $(this).closest('.project').find('.available').slideToggle();
+        $(this).toggleClass('plus minus');
+    });
+
+});
+
+// background moving
+var dir_h="right";
+var dir_v="down";
+function reset1(){
+	clearTimeout(my_time);
+	document.getElementById("i1").style.left="500px";
+	document.getElementById("i1").style.top="100px";
+	document.getElementById("msg").innerHTML=""
+}
+
+function disp(){
+	var b=1;var c=document.getElementById("i1").offsetTop;
+	var a=document.getElementById("i1").offsetLeft;
+	if(a>=-800){dir_h="left"}
+	if(a <=-920){dir_h="right" }
+	if(dir_h=="right" ){a=a+b}else{a=a-b}
+	if(c>=-1300){dir_v="up"}
+	if(c<=-1646){dir_v="down" }
+	if(dir_v=="down"){c=c+b}else{c=c-b}
+	document.getElementById("i1").style.top=c+ "px";
+	document.getElementById( "i1").style.left=a+ "px"}
+	
+function timer(){disp();
+	var b=document.getElementById( "i1").offsetTop;
+	var a=document.getElementById("i1").offsetLeft;my_time=setTimeout( "timer()",45)}
+
+//tabs
+window.addEventListener("load", function load() {
+	document.getElementById("defaultOpen").click();
+	});
+
+function openTab(evt, tabName) {
+	// Declare all variables
+	var i, tabcontent, tablinks;
+  
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+  
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+  
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+  }
